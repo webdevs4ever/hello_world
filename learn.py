4eebab7cc5308027b359131dfd55ad646615c697
RTF <rasheim.freeman@publicissapient.com>
2020-12-18 01:49:36 -0500

    this is second pirple assignment

--- learn.py
+++ learn.py
@@ -1,3 +1,61 @@
  ## Learn Python stuff goes here...
- PI = 3.1413
+ ## PI = 3.1413
+ ## print(PI)
+
+one = 1
+two = 2
+three = 3
+
+one,two,three = 1,2,3
+
+print(one)
+print(two)
+print(three)
+
+Decimal =1.1
+
+StringVar = "Hello" + "World"
+print (StringVar)
+
+def FunctionName():
+    newVar = "NewWorld"
+    print(one)
+    print(newVar)
+return
+
+print(three)
+
+Artist = "Sam Hunt"
+Song = "Hard To Forget"
+OriginalSource = 1957
+OriginalArtist = "Webb Pierce"
+Artwork = "HonkyTonk"
+Price = 2.50
+Platforms = "Apple, Spotify, Songza, YouTube"
+Genre = "CountryStrong"
+Duration = 300
+Producer = "Luke Laird"
+
+print(Artist)
+print(Song)
+print(OriginalSource)
+print(OriginalArtist)
+print(Artwork)
+print(Price)
+print(Platforms)
+print(Genre)
+print(Duration)
+print(Producer)
+
+
+"""
+"Oh baby, you're playing hard to forget"...
+Fun Fact #1 about the song "Hard to Forget" is that the original producer heard the song's signature line 
+playing from a jukebox and was immediately inspired by Kanye West's "808 and Heartbreak" style to create the
+final product--a country music smash.
+
+https://repl.it/@Rasheim/ModernOddballAfkgaming#main.py
+
+"""
+
  
