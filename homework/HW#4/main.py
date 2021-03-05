a8327f8b7030ed768a64e1b22acdc7a9596530b8
RTF <rasheim.freeman@publicissapient.com>
2021-03-05 18:48:17 -0500

    stuck answer for HW#4

--- homework/HW#4/main.py
+++ homework/HW#4/main.py
@@ -1,10 +1,10 @@
 ## HW for Python's lists in Chapter Four
 def addStuff(values):
-    myUniqueList = []
+    myUniqueList = [] ## creating empty list here
     
     for _ in range(values):
-      num=int(input("Enter a new number: "))
-      myUniqueList.append(num)
+      num=int(input("Enter a new number: ")) ## defining value as a number only
+      myUniqueList.append(num) 
     return myUniqueList
 
 print(addStuff(1))
